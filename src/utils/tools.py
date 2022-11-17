579784579e881476b9b8c494bf6d29bfdee32db3
Nicolas Cuadrado <me@nicolascuadrado.com>
2022-11-17 22:53:33 +0400

    :sparkles: Improved plot function
    Changed the function to plot multiple houses (microgrid setting). Still compatible with the SingleHouse run.

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -37,76 +37,110 @@ def plot_results(env, states, rewards, actions, net_energy, title, save=False, f
     states = np.stack(states, axis=0)
     net_energy = np.stack(net_energy, axis=0)
 
-    all_socs = states[:,:,:,-1]
+    # Custom configuration depending on the experiment setup (number of houses/actions)
 
-    # Plot results of interest
+    if len(rewards.shape) == 4:
+        single_house = True
+        n_houses = 1
+        all_socs = states[:,:,:,-1]
+    else:
+        single_house = False
+        n_houses = env.n_houses
+        all_socs = states[:,:,:,:,-1]
 
-    fig = plt.figure(figsize=(10, 15))
-    axs = fig.subplots(5, 2)
+    for ix_house in range(n_houses):
 
-    for ax_x in axs:
-        for ax_y in ax_x:
-            ax_y.minorticks_on()
-            ax_y.grid(True, which='both', axis='both', alpha=0.5)
+        house_name = f'House {ix_house + 1}'
 
-    axs[0][0].plot(rewards[0,:,:].mean(axis=1), label='0%')
-    axs[0][0].plot(rewards[quart_index,:,:].mean(axis=1), label='25%')
-    axs[0][0].plot(rewards[mid_index,:,:].mean(axis=1), label='50%')
-    axs[0][0].plot(rewards[three_quart_index,:,:].mean(axis=1), label='75%')
-    axs[0][0].plot(rewards[last_index,:,:].mean(axis=1), label='100%')
-    axs[0][0].set_title('Mean reward through time')
-    axs[0][0].legend()
+        # Plot results of interest
 
-    axs[0][1].plot(rewards.sum(axis=1).mean(axis=1), label='Reward')
-    axs[0][1].set_title('Mean reward through epochs')
-    axs[0][1].legend()
+        fig = plt.figure(figsize=(10, 15), num=house_name)
+        axs = fig.subplots(5, 2)
 
-    axs[1][0].plot(actions[0, :, :].mean(axis=1), label='0%')
-    axs[1][0].plot(actions[quart_index, :, :].mean(axis=1), label='25%')
-    axs[1][0].plot(actions[mid_index, :, :].mean(axis=1), label='50%')
-    axs[1][0].plot(actions[three_quart_index, :, :].mean(axis=1), label='75%')
-    axs[1][0].plot(actions[last_index, :, :].mean(axis=1), label='100%')
-    axs[1][0].set_title('Mean action through time')
-    axs[1][0].legend()
+        for ax_x in axs:
+            for ax_y in ax_x:
+                ax_y.minorticks_on()
+                ax_y.grid(True, which='both', axis='both', alpha=0.5)
 
-    axs[1][1].plot(actions.mean(axis=1).mean(axis=-1), label='Action')
-    axs[1][1].legend()
-    axs[1][1].set_title('Mean action through epochs')
+        # Compute plots for the current house
 
-    axs[2][0].plot(all_socs[0, :, :].mean(axis=1), label='0%')
-    axs[2][0].plot(all_socs[quart_index, :, :].mean(axis=1), label='25%')
-    axs[2][0].plot(all_socs[mid_index, :, :].mean(axis=1), label='50%')
-    axs[2][0].plot(all_socs[three_quart_index, :, :].mean(axis=1), label='75%')
-    axs[2][0].plot(all_socs[last_index, :, :].mean(axis=1), label='100%')
-    axs[2][0].legend()
-    axs[2][0].set_title('Mean SOC through time')
+        mean_reward_time = rewards.mean(axis=-2)
+        mean_reward_epochs = rewards.sum(axis=1).mean(axis=-2)
 
-    axs[2][1].plot(all_socs.mean(axis=1).mean(axis=-1), label='SOC')
-    axs[2][1].set_title('Mean SOC through epochs')
-    axs[2][1].legend()
+        mean_action_time = actions.mean(axis=-1)
+        mean_action_epochs = actions.sum(axis=1).mean(axis=-1)
 
-    axs[3][0].plot(net_energy[0, :, :].mean(axis=0), label='0%')
-    axs[3][0].plot(net_energy[quart_index, :, :].mean(axis=0), label='25%')
-    axs[3][0].plot(net_energy[mid_index, :, :].mean(axis=0), label='50%')
-    axs[3][0].plot(net_energy[three_quart_index, :, :].mean(axis=0), label='75%')
-    axs[3][0].plot(net_energy[last_index, :, :].mean(axis=0), label='100%')
-    axs[3][0].plot((env.mg.demand - env.mg.pv_gen), label='Remaining', linestyle='--', linewidth=2)
-    axs[3][0].legend()
-    axs[3][0].set_title('Mean net energy through time')
+        mean_soc_time = all_socs.mean(axis=-1)
+        mean_soc_epochs = all_socs.mean(axis=1).mean(axis=-1)
 
-    axs[3][1].plot(net_energy.mean(axis=1).sum(axis=-1), label='Net energy')
-    axs[3][1].set_title('Mean net energy through epochs')
-    axs[3][1].legend()
+        mean_net_energy_time = net_energy.mean(axis=-2)
+        mean_net_energy_epochs = net_energy.mean(axis=-2).sum(axis=-1)
 
-    axs[4][0].plot(env.mg.pv_gen, label='PV')
-    axs[4][0].plot(env.mg.demand, label='Demand')
-    axs[4][0].set_title('PV and Demand')
-    axs[4][0].legend()
+        demand = env.mg.demand if single_house else env.mg.houses[ix_house].demand
+        pv_gen = env.mg.pv_gen if single_house else env.mg.houses[ix_house].pv_gen
+        price = env.mg.price if single_house else env.mg.houses[ix_house].price
+        emission = env.mg.emission if single_house else env.mg.houses[ix_house].emission
 
-    axs[4][1].plot(env.mg.price, label='Price')
-    axs[4][1].plot(env.mg.emission, label='Emission factor')
-    axs[4][1].set_title('Price and Emission factor')
-    axs[4][1].legend()
+        # Plot results
+
+        axs[0][0].plot(mean_reward_time[0,:,ix_house], label='0%')
+        axs[0][0].plot(mean_reward_time[quart_index,:,ix_house], label='25%')
+        axs[0][0].plot(mean_reward_time[mid_index,:,ix_house], label='50%')
+        axs[0][0].plot(mean_reward_time[three_quart_index,:,ix_house], label='75%')
+        axs[0][0].plot(mean_reward_time[last_index,:,ix_house], label='100%')
+        axs[0][0].set_title('Mean reward through time')
+        axs[0][0].legend()
+        
+        axs[0][1].plot(mean_reward_epochs[:,ix_house], label='Reward')
+        axs[0][1].set_title('Mean accumulated reward through epochs')
+        axs[0][1].legend()
+        
+        axs[1][0].plot(mean_action_time[0, :, None if single_house else ix_house], label='0%')
+        axs[1][0].plot(mean_action_time[quart_index, :, None if single_house else ix_house], label='25%')
+        axs[1][0].plot(mean_action_time[mid_index, :, None if single_house else ix_house], label='50%')
+        axs[1][0].plot(mean_action_time[three_quart_index, :, None if single_house else ix_house], label='75%')
+        axs[1][0].plot(mean_action_time[last_index, :, None if single_house else ix_house], label='100%')
+        axs[1][0].set_title('Mean action through time')
+        axs[1][0].legend()
+
+        axs[1][1].plot(mean_action_epochs[:, None if single_house else ix_house], label='Action')
+        axs[1][1].legend()
+        axs[1][1].set_title('Mean action through epochs')
+
+        axs[2][0].plot(mean_soc_time[0, :, None if single_house else ix_house], label='0%')
+        axs[2][0].plot(mean_soc_time[quart_index, :, None if single_house else ix_house], label='25%')
+        axs[2][0].plot(mean_soc_time[mid_index, :, None if single_house else ix_house], label='50%')
+        axs[2][0].plot(mean_soc_time[three_quart_index, :, None if single_house else ix_house], label='75%')
+        axs[2][0].plot(mean_soc_time[last_index, :, None if single_house else ix_house], label='100%')
+        axs[2][0].legend()
+        axs[2][0].set_title('Mean SOC through time')
+
+        axs[2][1].plot(mean_soc_epochs[:,None if single_house else ix_house], label='SOC')
+        axs[2][1].set_title('Mean SOC through epochs')
+        axs[2][1].legend()
+
+        axs[3][0].plot(mean_net_energy_time[0, None if single_house else ix_house, :].T, label='0%')
+        axs[3][0].plot(mean_net_energy_time[quart_index, None if single_house else ix_house, :].T, label='25%')
+        axs[3][0].plot(mean_net_energy_time[mid_index, None if single_house else ix_house, :].T, label='50%')
+        axs[3][0].plot(mean_net_energy_time[three_quart_index, None if single_house else ix_house, :].T, label='75%')
+        axs[3][0].plot(mean_net_energy_time[last_index, None if single_house else ix_house, :].T, label='100%')
+        axs[3][0].plot((demand - pv_gen), label='Remaining', linestyle='--', linewidth=2)
+        axs[3][0].legend()
+        axs[3][0].set_title('Mean net energy through time')
+
+        axs[3][1].plot(mean_net_energy_epochs[:, None if single_house else ix_house], label='Net energy')
+        axs[3][1].set_title('Mean net energy through epochs')
+        axs[3][1].legend()
+
+        axs[4][0].plot(pv_gen, label='PV')
+        axs[4][0].plot(demand, label='Demand')
+        axs[4][0].set_title('PV and Demand')
+        axs[4][0].legend()
+
+        axs[4][1].plot(price, label='Price')
+        axs[4][1].plot(emission, label='Emission factor')
+        axs[4][1].set_title('Price and Emission factor')
+        axs[4][1].legend()
     
     fig.suptitle(title)
     fig.tight_layout()
