7cd20ada961ddde490b525b1538d6215fb49493a
Nicolas Cuadrado <me@nicolascuadrado.com>
2023-03-23 14:10:56 +0400

    :adhesive_bandage: Include test score in the results comparison
    We were classifying the results according to the training score but then looking and the test score it was worse. Now we classify according to the test score.

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -28,6 +28,10 @@ def load_config(config_name: str="d_a2c_mg"):
 
 def plot_metrics(metrics, save: bool = False, filename: str = "metrics"):
     
+    font = {'family' : 'normal', 'size'   : 12}
+
+    plt.rc('font', **font)
+
     # Parse metrics
 
     t_price_metric = np.stack(metrics['train']['agent']['price_metric'], axis=0)
@@ -45,6 +49,8 @@ def plot_metrics(metrics, save: bool = False, filename: str = "metrics"):
         ax.grid(True, which='both', axis='both', alpha=0.5)
     
     axs[0].plot(t_price_metric, label='Training')
+    # std = np.std(t_price_metric)
+    # axs[0].fill_between(x=range(len(t_price_metric)), y1=t_price_metric - std, y2=t_price_metric + std, color='blue', alpha=0.2, label='Standard Deviation')
     axs[0].plot(e_price_metric, label='Evaluation')
     # axs[0].plot(tst_price_metric, label='Testing')
     axs[0].set_title('Price')
@@ -56,27 +62,28 @@ def plot_metrics(metrics, save: bool = False, filename: str = "metrics"):
     # axs[1].plot(tst_emissions_metric, label='Testing')
     axs[1].set_title('Emissions')
     axs[1].set_xlabel('Epoch')
-    axs[1].set_ylabel('kgCO2')
+    axs[1].set_ylabel('CO2')
 
     # Get the step size for the patches
 
-    step = int(load_config()['train']['env']['switch_steps'])
+    # step = int(load_config()['train']['env']['switch_steps'])
+    step = int(len(t_price_metric)/12)
 
     # Patches to show the epochs where the grid profile changes # TODO: Make this more general and automated
 
-    prof_1_1 = patches.Rectangle((0, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightblue')
-    prof_2_1 = patches.Rectangle((step * 1, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgreen')
-    prof_3_1 = patches.Rectangle((step * 2, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgoldenrodyellow')
-    prof_4_1 = patches.Rectangle((step * 3, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightpink')
-    prof_5_1 = patches.Rectangle((step * 4, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgray')
-    prof_6_1 = patches.Rectangle((step * 5, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightsteelblue')
+    prof_1_1 = patches.Rectangle((0, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightblue', alpha=0.5)
+    prof_2_1 = patches.Rectangle((step * 1, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgreen', alpha=0.5)
+    prof_3_1 = patches.Rectangle((step * 2, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgoldenrodyellow', alpha=0.5)
+    prof_4_1 = patches.Rectangle((step * 3, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightpink', alpha=0.5)
+    prof_5_1 = patches.Rectangle((step * 4, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgray', alpha=0.5)
+    prof_6_1 = patches.Rectangle((step * 5, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightsteelblue', alpha=0.5)
 
-    prof_1_2 = patches.Rectangle((step * 6, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightblue')
-    prof_2_2 = patches.Rectangle((step * 7, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgreen')
-    prof_3_2 = patches.Rectangle((step * 8, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgoldenrodyellow')
-    prof_4_2 = patches.Rectangle((step * 9, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightpink')
-    prof_5_2 = patches.Rectangle((step * 10, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgray')
-    prof_6_2 = patches.Rectangle((step * 11, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightsteelblue')
+    prof_1_2 = patches.Rectangle((step * 6, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightblue', alpha=0.5)
+    prof_2_2 = patches.Rectangle((step * 7, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgreen', alpha=0.5)
+    prof_3_2 = patches.Rectangle((step * 8, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgoldenrodyellow', alpha=0.5)
+    prof_4_2 = patches.Rectangle((step * 9, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightpink', alpha=0.5)
+    prof_5_2 = patches.Rectangle((step * 10, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightgray', alpha=0.5)
+    prof_6_2 = patches.Rectangle((step * 11, t_price_metric.min()), step, t_price_metric.max() - t_price_metric.min(), color='lightsteelblue', alpha=0.5)
 
     # Add all the patches to the plot
 
@@ -94,19 +101,19 @@ def plot_metrics(metrics, save: bool = False, filename: str = "metrics"):
     axs[0].add_patch(prof_5_2)
     axs[0].add_patch(prof_6_2)
 
-    prof_1_1 = patches.Rectangle((0, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightblue')
-    prof_2_1 = patches.Rectangle((step * 1, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgreen')
-    prof_3_1 = patches.Rectangle((step * 2, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgoldenrodyellow')
-    prof_4_1 = patches.Rectangle((step * 3, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightpink')
-    prof_5_1 = patches.Rectangle((step * 4, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgray')
-    prof_6_1 = patches.Rectangle((step * 5, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightsteelblue')
+    prof_1_1 = patches.Rectangle((0, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightblue', alpha=0.5)
+    prof_2_1 = patches.Rectangle((step * 1, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgreen', alpha=0.5)
+    prof_3_1 = patches.Rectangle((step * 2, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgoldenrodyellow', alpha=0.5)
+    prof_4_1 = patches.Rectangle((step * 3, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightpink', alpha=0.5)
+    prof_5_1 = patches.Rectangle((step * 4, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgray', alpha=0.5)
+    prof_6_1 = patches.Rectangle((step * 5, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightsteelblue', alpha=0.5)
 
-    prof_1_2 = patches.Rectangle((step * 6, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightblue')
-    prof_2_2 = patches.Rectangle((step * 7, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgreen')
-    prof_3_2 = patches.Rectangle((step * 8, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgoldenrodyellow')
-    prof_4_2 = patches.Rectangle((step * 9, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightpink')
-    prof_5_2 = patches.Rectangle((step * 10, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgray')
-    prof_6_2 = patches.Rectangle((step * 11, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightsteelblue')
+    prof_1_2 = patches.Rectangle((step * 6, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightblue', alpha=0.5)
+    prof_2_2 = patches.Rectangle((step * 7, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgreen', alpha=0.5)
+    prof_3_2 = patches.Rectangle((step * 8, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgoldenrodyellow', alpha=0.5)
+    prof_4_2 = patches.Rectangle((step * 9, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightpink', alpha=0.5)
+    prof_5_2 = patches.Rectangle((step * 10, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightgray', alpha=0.5)
+    prof_6_2 = patches.Rectangle((step * 11, t_emissions_metric.min()), step, t_emissions_metric.max() - t_emissions_metric.min(), color='lightsteelblue', alpha=0.5)
 
     axs[1].add_patch(prof_1_1)
     axs[1].add_patch(prof_2_1)
@@ -122,6 +129,69 @@ def plot_metrics(metrics, save: bool = False, filename: str = "metrics"):
     axs[1].add_patch(prof_5_2)
     axs[1].add_patch(prof_6_2)
 
+    tr_opt_price_metric = np.zeros(t_price_metric.shape)
+    tr_opt_price_metric[0:step] = -0.019839614867877792
+    tr_opt_price_metric[step:step*2] = -0.2598215598473279
+    tr_opt_price_metric[step*2:step*3] = -0.2030865518737324
+    tr_opt_price_metric[step*3:step*4] = -0.02006871725852033
+    tr_opt_price_metric[step*4:step*5] = -0.22021958548012321
+    tr_opt_price_metric[step*5:step*6] = -0.03440127418544243
+    tr_opt_price_metric[step*6:step*7] = -0.019839614867877792
+    tr_opt_price_metric[step*7:step*8] = -0.2598215598473279
+    tr_opt_price_metric[step*8:step*9] = -0.2030865518737324
+    tr_opt_price_metric[step*9:step*10] = -0.02006871725852033
+    tr_opt_price_metric[step*10:step*11] = -0.22021958548012321
+    tr_opt_price_metric[step*11:step*12] = -0.03440127418544243
+
+    axs[0].plot(tr_opt_price_metric, label='Training opt.', linestyle='--')
+
+    tr_opt_emissions_metric = np.zeros(t_price_metric.shape)
+    tr_opt_emissions_metric[0:step] = -0.0869708425652896
+    tr_opt_emissions_metric[step:step*2] = -0.5135502954614409
+    tr_opt_emissions_metric[step*2:step*3] = -0.4071098041551269
+    tr_opt_emissions_metric[step*3:step*4] = -0.0588465767104367
+    tr_opt_emissions_metric[step*4:step*5] = -0.5216119995482944
+    tr_opt_emissions_metric[step*5:step*6] = -0.1363736307291066
+    tr_opt_emissions_metric[step*6:step*7] = -0.0869708425652896
+    tr_opt_emissions_metric[step*7:step*8] = -0.5135502954614409
+    tr_opt_emissions_metric[step*8:step*9] = -0.4071098041551269
+    tr_opt_emissions_metric[step*9:step*10] = -0.0588465767104367
+    tr_opt_emissions_metric[step*10:step*11] = -0.5216119995482944
+    tr_opt_emissions_metric[step*11:step*12] = -0.1363736307291066
+
+    axs[1].plot(tr_opt_emissions_metric, label='Training opt.', linestyle='--')
+
+    val_opt_price_metric = np.zeros(t_price_metric.shape)
+    val_opt_price_metric[0:step] = -0.039756478733823215
+    val_opt_price_metric[step:step*2] = -0.16442607984860486
+    val_opt_price_metric[step*2:step*3] = -0.16932370350198336
+    val_opt_price_metric[step*3:step*4] = -0.03386473927377796
+    val_opt_price_metric[step*4:step*5] = -0.1983250166612216
+    val_opt_price_metric[step*5:step*6] = 0.012034441751068388
+    val_opt_price_metric[step*6:step*7] = -0.039756478733823215
+    val_opt_price_metric[step*7:step*8] = -0.16442607984860486
+    val_opt_price_metric[step*8:step*9] = -0.16932370350198336
+    val_opt_price_metric[step*9:step*10] = -0.03386473927377796
+    val_opt_price_metric[step*10:step*11] = -0.1983250166612216
+    val_opt_price_metric[step*11:step*12] = 0.012034441751068388
+
+    axs[0].plot(val_opt_price_metric, label='Val. opt.', linestyle='--')
+
+    val_opt_emissions_metric = np.zeros(t_price_metric.shape)
+    val_opt_emissions_metric[0:step] = -0.09671977260413123
+    val_opt_emissions_metric[step:step*2] = -0.36373597292609544
+    val_opt_emissions_metric[step*2:step*3] = -0.3446213670851952
+    val_opt_emissions_metric[step*3:step*4] = -0.06899305358916666
+    val_opt_emissions_metric[step*4:step*5] = -0.5338496535295544
+    val_opt_emissions_metric[step*5:step*6] = -0.08116904715138697
+    val_opt_emissions_metric[step*6:step*7] = -0.09671977260413123
+    val_opt_emissions_metric[step*7:step*8] = -0.36373597292609544
+    val_opt_emissions_metric[step*8:step*9] = -0.3446213670851952
+    val_opt_emissions_metric[step*9:step*10] = -0.06899305358916666
+    val_opt_emissions_metric[step*10:step*11] = -0.5338496535295544
+    val_opt_emissions_metric[step*11:step*12] = -0.08116904715138697
+
+    axs[1].plot(val_opt_emissions_metric, label='Val. opt.', linestyle='--')
 
     # Review if cvxpy metrics are available
     
